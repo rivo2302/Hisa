9557ded32b5993154d74bf824ff9b24b6f6b60c3
rivo2302@gmail.com <rivo2302@gmail.com>
2022-03-30 00:07:34 +0300

    [IMP] Jusqu'au chiffre 100 milliards

--- isa.py
+++ isa.py
@@ -14,8 +14,9 @@ class Isa :
        The class that contains each method of the model.
     """
     def __init__(self,item) :
-        self.tokana = ["aotra","ray","roa","telo","efatra","dimy","enina","fito","valo","sivy","folo"]
-        self.folo = ["","folo","roapolo","telopolo","efapolo","dimampolo","enimpolo","fitopolo","valopolo","sivy folo"]
+        self.tokana = ["","ray","roa","telo","efatra","dimy","enina","fito","valo","sivy","folo"]
+        self.folo = ["","folo","roapolo","telopolo","efapolo","dimampolo","enimpolo","fitopolo","valopolo","sivifolo"]
+        self.zato = ["","zato","roanjato","telonjato","efajato","dimanjato","eninjato","fitonjato","valonjato","sivanjato"]
 
         if str(item).isdigit() == False :
             print(f"Error  : The argument must Isa(args) be an integer")
@@ -45,17 +46,58 @@ class Isa :
         return res
 
     def __iray(self , nb : int) -> str:
+        if nb == 0 : return "aotra"
         return (self.tokana[nb])
     
     def __roa (self , nb : int) -> str :
-        if (nb%10) == 0 : return self.tokana[nb]
+        if (nb%10) == 0 : return self.folo[nb//10]
+        elif (int(nb//10) == 1) : 
+            if nb%10 == 1 : return "iraika ambiny folo"
+            return f"{self.tokana[nb%10]} ambiny folo"
+        return f"{self.tokana[nb%10]} amby {self.folo[nb//10]}"
     
+    def __telo (self , nb : int) -> str :
+        if nb%100 ==  0 : return self.zato[nb//100]
+        x = Isa(nb%100)
+        if nb // 100 == 1 :
+            return (f"{x} amby zato")
+        return (f"{x} sy {self.zato[nb//100]}")
+        
+    def __efatra (self , nb : int) -> str : 
+        if nb%1000 == 0 :
+            if nb//1000 == 1 : return "arivo"
+            else : return f"{self.tokana[nb//1000]} arivo"
+        x = Isa(nb%1000)
+        if nb//1000 == 1 : return f"{x} sy arivo"
+        return f"{x} sy {self.tokana[nb//1000]} arivo"
+
+    def __dimy (self , nb : int) -> str : 
+        if nb%10000 == 0 : return f"{self.tokana[nb//10000]} alina"
+        return f"{Isa(nb%10000)} sy {self.tokana[nb//10000]} alina"
+
+    def __enina (self , nb : int) -> str : 
+        if nb%100000 == 0 : return f"{self.tokana[nb//100000]} hetsy"
+        return f"{Isa(nb%100000)} sy {self.tokana[nb//100000]} hetsy"
+
+    def __fito (self , nb : int) -> str : 
+        if nb%1000000 == 0 : return f"{Isa(nb//1000000)} tapitrisa"
+        return f"{Isa(nb%1000000)} sy {Isa(nb//1000000)} tapitrisa"
+
+    def __valo (self , nb : int) -> str : 
+        if nb%1000000 == 0 : return f"{Isa(nb//1000000)} tapitrisa"
+        return f"{Isa(nb%1000000)} sy {Isa(nb//1000000)} tapitrisa"
+        
     def main (self , nb : int) -> str :
         if self.__length(nb) == 1 : res = self.__iray(nb)
         elif self.__length(nb) == 2 : res = self.__roa(nb)
-        return res.capitalize()
+        elif self.__length(nb) == 3 : res = self.__telo(nb)
+        elif self.__length(nb) == 4 : res = self.__efatra(nb)
+        elif self.__length(nb) == 5 : res = self.__dimy(nb)
+        elif self.__length(nb) == 6 : res = self.__enina(nb)
+        elif self.__length(nb) == 7 : res = self.__fito(nb)
+        elif self.__length(nb) == 8 : res = self.__valo(nb)
+        return res
 
-            
-
-rivo = Isa("10")
+    
+rivo = Isa("100000000")
 print(rivo)
