cff77db33067f531cf97ab4d00bd1d7d748013c4
rivo2302@gmail.com <rivo2302@gmail.com>
2022-03-29 22:40:25 +0300

    [IMP] <10é é

--- isa.py
+++ isa.py
@@ -14,9 +14,40 @@ class Isa :
        The class that contains each method of the model.
     """
     def __init__(self,item) :
-        if item.isdigit() == False :
-            print(f"Error : The argument must be an integer")
+        self.tokana = ["aotra","ray","roa","telo","efatra","dimy","enina","fito","valo","sivy"]
+        if str(item).isdigit() == False :
+            print(f"Error  : The argument must Isa(args) be an integer")
             sys.exit()
+        self.item = int(item)
+    def __str__(self) :
+        return self.__length(45)
 
+    def __length(self, integer : int ) -> int:
+        """
+            Method that calculates the length of the number.
+        Args:
+            integer (_integer_): _description_
 
-rivo = Isa("79")
+        Returns:
+            _integer_: _len of the number in args_
+        """
+        if integer < 10 : res = 1
+        elif integer < 100 : res = 2
+        elif integer < 1000 : res = 3 
+        elif integer < 10000 : res = 4
+        elif integer < 100000 : res = 5
+        elif integer < 1000000 : res = 6
+        elif integer < 1000000000 : res = 7
+        elif integer < 1000000000000 : res = 8
+        return res
+
+    def __iray(self , nb : int) -> str:
+        return (self.tokana[int])
+
+    
+    def main (self , integer : int) -> str :
+        if self.__length(integer) == 0 : return self.__iray(integer)
+
+            
+
+rivo = Isa("75")
